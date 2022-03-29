3dd4683a0d0f70dfa9b02775d20f27c86cccb2ac
Will Robbins <williamrbs1996@gmail.com>
2022-03-29 09:51:34 -0300

    able to sign in. had to hardcode aws-exports.js values

--- bonappetit/src/App.js
+++ bonappetit/src/App.js
@@ -1,27 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
+import { Auth, Hub } from 'aws-amplify';
 
 
 function App() {
+
+  const [user, updateUser] = useState(null)
+
+  useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then(user => updateUser(user))
+      .catch(() => console.log("No signed in user..."))
+
+    Hub.listen('auth', data => {
+      switch (data.payload.event) {
+        case 'signIn':
+          return updateUser(data.payload.data)
+        case 'signOut':
+          return updateUser(null);
+      }
+    })
+  }, [])
+
+  if (user) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AmplifyAuthenticator>
+      <AmplifySignUp slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" }
+        ]}
+      />
+    </AmplifyAuthenticator>
+  )
+
 }
 
 export default App;
--- bonappetit/src/aws-exports.js
+++ bonappetit/src/aws-exports.js
@@ -2,12 +2,12 @@ import BonAppetitBackendStack from './cdk-exports.json'
 
 const config = {
   Auth: {
-    region: BonAppetitBackendStack.projectregion,
-    userPoolId: BonAppetitBackendStack.userpoolid,
-    userPoolWebClientId: BonAppetitBackendStack.userpoolclientid
+    region: "us-east-1",
+    userPoolId: "us-east-1_CCzzINuiA",
+    userPoolWebClientId: "2m2c218c3908rukd7c8sa43kbd"
   },
-  aws_appsync_graphqlEndpoint: BonAppetitBackendStack.bonappetitapiendpoint,
-  aws_appsync_region: BonAppetitBackendStack.projectregion,
+  aws_appsync_graphqlEndpoint: "https://t6muvvpwdzehpejmkkhya23tme.appsync-api.us-east-1.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-1",
   aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS"
 }
 
