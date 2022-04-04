f8329f1f34d76bf9a64b1da244c9b0578c209db7
Will Robbinsexit <williamrbs1996@gmail.com>
2022-04-04 17:55:37 -0300

    cdk keys updated, ready for prod

--- bonappetit/src/aws-exports.js
+++ bonappetit/src/aws-exports.js
@@ -3,10 +3,10 @@ import BonAppetitBackendStack from './cdk-exports.json'
 const config = {
   Auth: {
     region: "us-east-1",
-    userPoolId: "us-east-1_r4HmVe7QV",
-    userPoolWebClientId: "7cskd7pouapt736dv49ellrao4"
+    userPoolId: "us-east-1_N3dfOvhEI",
+    userPoolWebClientId: "49a7b2sq48d7ahnbpuvd5c4utp"
   },
-  aws_appsync_graphqlEndpoint: "https://dhl5c26cqvafrfpnwkyxahlham.appsync-api.us-east-1.amazonaws.com/graphql",
+  aws_appsync_graphqlEndpoint: "https://awrqjlsd5jdhjiagqdzr5g7ili.appsync-api.us-east-1.amazonaws.com/graphql",
   aws_appsync_region: "us-east-1",
   aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS"
 }
